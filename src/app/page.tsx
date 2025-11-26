cc49853f4aa600b391d707c2f76adfbd59a98fff
Anuj Sharma <anujsharmatalks@gmail.com>
2025-11-26 14:58:16 +0000

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { AboutSection } from '@/components/about-section';
 import { HeroSection } from '@/components/hero-section';
+import { ServicesSection } from '@/components/services-section';
 
 export default function Home() {
   return (
     <main>
       <HeroSection />
       <AboutSection />
+      <ServicesSection />
     </main>
   );
 }
