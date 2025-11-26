b43202888fa16931400a4f13c24ca4e08428e7ca
Anuj Sharma <anujsharmatalks@gmail.com>
2025-11-26 10:04:58 +0000

    make layout to fix this over layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { HeroSection } from "@/components/hero-section";
 
 export default function Home() {
   return (
-    <main>
+    <main className="flex min-h-screen flex-col items-center justify-center">
       <HeroSection />
     </main>
   );
