c0137321ac005c6b0c7b88c44953c2880a511e13
Anuj Sharma <anujsharmatalks@gmail.com>
2025-11-26 10:26:52 +0000

    change hero section desing with this design 'use client' import { Button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,6 @@ import { HeroSection } from "@/components/hero-section";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center">
-      <HeroSection />
-    </main>
+    <HeroSection />
   );
 }
